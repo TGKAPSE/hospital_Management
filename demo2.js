decce9057a663690c1c28f7e3fc804dcd6d16e1f
Tanmay Kapse <127239744+TGKAPSE@users.noreply.github.com>
2023-03-15 16:11:58 +0530

    Add files via upload

---
+++ demo2.js
@@ -0,0 +1,13 @@
+
+function tanmay(){
+var username=document.getElementById("username").value
+var password=document.getElementById("password").value
+    if(password=="")
+    {
+        alert("password can not be empty")
+    }
+    if(username=="")
+    {
+        alert("username can not be empty")
+    }
+}
